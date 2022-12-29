f7cc455bd572ff49b859641e0b007d10b345fdbd
Mihail Golubin <golm2020@gmail.com>
2022-12-30 01:22:39 +0300

    get formData added

--- js/modules/counter.js
+++ js/modules/counter.js
@@ -28,8 +28,10 @@ export default class Counter {
   constructor(element) {
     this.element = element;
     this.btnReset = element.querySelector(".form__reset-button");
+    this.btnSubmit = element.querySelector(".form__submit-button");
     this.form = element.querySelector(".form");
     this.genderMale = element.querySelector("#gender-male");
+    this.activity = element.querySelector(".radio");
 
     this.age = element.querySelector("#age");
     this.height = element.querySelector("#height");
@@ -39,39 +41,36 @@ export default class Counter {
   }
 
   _onFormInput() {
-    console.log(this.element);
+    this.ageInt = Math.abs(parseInt(this.age.value));
+    this.heightInt = Math.abs(parseInt(this.height.value));
+    this.weightInt = Math.abs(parseInt(this.weight.value));
 
-    this.ageInt = parseInt(this.age.value);
-    this.heightInt = parseInt(this.height.value);
-    this.weightInt = parseInt(this.weight.value);
     this.btnReset.disabled = !(this.ageInt || this.heightInt || this.weightInt); //button reset is enabled
-    this.genderIsMale = this.genderMale.checked;
+    this.btnSubmit.disabled = !Boolean(
+      this.ageInt && this.heightInt && this.weightInt
+    ); //button form__submit is enabled
 
-    console.log(
-      `Value of age, height, weight: ${this.ageInt}, ${this.heightInt}, ${this.weightInt}`
-    );
-    console.log(
-      `typeof of age, height, weight: ${typeof this.ageInt}, ${typeof this
-        .heightInt}, ${typeof this.weightInt}`
-    );
-    console.log(
-      `Boolen of age, height, weight: ${Boolean(this.ageInt)}, ${Boolean(
-        this.heightInt
-      )}, ${Boolean(this.weightInt)}`
-    );
-
+    this.physicalActivity = this.element.querySelector(
+      'input[name="activity"]:checked'
+    ).value;
+    console.log(`activity ${this.physicalActivity}`);
     // получение данных от пользователя
     // также можно добавить небольшую валидацию
   }
 
   _onFormReset() {
     this.form.reset(); // reset the form
-    this.btnReset.disabled = true; //button reset is disabled
-
+    this.btnSubmit.disabled = this.btnReset.disabled = true; //buttons reset, submit were disabled
     // задизабленность при обновлении страницы кнопок, скрытие блока с результатом
   }
 
   _onFormSubmit(evt) {
+    console.log(evt);
+    console.log(evt.get("activity"));
+    console.log(evt.get("gender"));
+    console.log(evt.get("age"));
+    console.log(evt.get("height"));
+
     // вызов методов расчета калорий
     // getCaloriesNorm(), getCaloriesMin(), getCaloriesMax()
     // показ блока с результатами калорий
@@ -80,16 +79,20 @@ export default class Counter {
   init() {
     this.element.addEventListener("change", (event) => {
       event.preventDefault();
-      this._onFormInput(); //
-      // console.log("======");
-      // if (this.age || this.weight || this.height) {
-      //   this.btnReset.removeAttribute("disabled");
-      //   console.log(`Element is enabled`);
-      // }
+      this._onFormInput();
     });
     this.btnReset.addEventListener("click", (event) => {
       event.preventDefault();
-      this._onFormReset(); // Дуйствия при нажатии сброс
+      this._onFormReset();
+    });
+    this.form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      this.formData = new FormData(this.form);
+      console.log(`FormData activity = ${this.formData.get("activity")}`);
+      console.log(`FormData gender = ${this.formData.get("gender")}`);
+      console.log(`FormData age = ${this.formData.get("age")}`);
+      console.log(`FormData height = ${this.formData.get("height")}`);
+      this._onFormSubmit(this.formData);
     });
 
     // инициализация обработчиков событий
