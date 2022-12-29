88f1f017f0c33727497baf6805e0e14619e4ae16
Mihail Golubin <golm2020@gmail.com>
2022-12-29 22:28:22 +0300

    Добавлено скрытие/отображение кнопки reset

--- js/modules/counter.js
+++ js/modules/counter.js
@@ -1,69 +1,112 @@
-import Result from './result.js';
+import Result from "./result.js";
 
 const PhysicalActivityRatio = {
-    MIN: 1.2,
-    LOW: 1.375,
-    MEDIUM: 1.55,
-    HIGH: 1.725,
-    MAX: 1.9,
+  MIN: 1.2,
+  LOW: 1.375,
+  MEDIUM: 1.55,
+  HIGH: 1.725,
+  MAX: 1.9,
 };
 
 const CaloriesFormulaFactor = {
-    AGE: 5,
-    WEIGHT: 10,
-    HEIGHT: 6.25,
+  AGE: 5,
+  WEIGHT: 10,
+  HEIGHT: 6.25,
 };
 
 const CaloriesFormulaConstant = {
-    MALE: -5,
-    FEMALE: 161
+  MALE: -5,
+  FEMALE: 161,
 };
 
 const CaloriesMinMaxRatio = {
-    MIN: 0.85,
-    MAX: 1.15
+  MIN: 0.85,
+  MAX: 1.15,
 };
 
 export default class Counter {
-    constructor(element) {
-        // перечисление параметров, необходимых для работы: gender, age, weight, height, activity и т.д.
-    }
+  constructor(element) {
+    this.element = element;
+    this.btnReset = element.querySelector(".form__reset-button");
+    this.form = element.querySelector(".form");
+    this.genderMale = element.querySelector("#gender-male");
 
-    _onFormInput() {
-        // получение данных от пользователя
-        // также можно добавить небольшую валидацию
-    }
+    this.age = element.querySelector("#age");
+    this.height = element.querySelector("#height");
+    this.weight = element.querySelector("#weight");
 
-    _onFormReset() {
-        // задизабленность при обновлении страницы кнопок, скрытие блока с результатом
-    }
+    // перечисление параметров, необходимых для работы: gender, age, weight, height, activity и т.д.
+  }
 
-    _onFormSubmit(evt) {
-        // вызов методов расчета калорий 
-        // getCaloriesNorm(), getCaloriesMin(), getCaloriesMax()
-        // показ блока с результатами калорий
-    }
+  _onFormInput() {
+    console.log(this.element);
 
-    init() {
-        // инициализация обработчиков событий 
-        // _onFormInput, _onFormReset, _onFormSubmit
-    }
+    this.ageInt = parseInt(this.age.value);
+    this.heightInt = parseInt(this.height.value);
+    this.weightInt = parseInt(this.weight.value);
+    this.btnReset.disabled = !(this.ageInt || this.heightInt || this.weightInt); //button reset is enabled
+    this.genderIsMale = this.genderMale.checked;
 
-    deinit() {
-        // удаление обработчиков событий 
-        // _onFormInput, _onFormReset, _onFormSubmit
-    }
+    console.log(
+      `Value of age, height, weight: ${this.ageInt}, ${this.heightInt}, ${this.weightInt}`
+    );
+    console.log(
+      `typeof of age, height, weight: ${typeof this.ageInt}, ${typeof this
+        .heightInt}, ${typeof this.weightInt}`
+    );
+    console.log(
+      `Boolen of age, height, weight: ${Boolean(this.ageInt)}, ${Boolean(
+        this.heightInt
+      )}, ${Boolean(this.weightInt)}`
+    );
 
-    getCaloriesNorm() {
-        // перечисление констант age, weight, height, gender, activity
-        // применение формулы расчета
-    }
+    // получение данных от пользователя
+    // также можно добавить небольшую валидацию
+  }
 
-    getCaloriesMin() {
+  _onFormReset() {
+    this.form.reset(); // reset the form
+    this.btnReset.disabled = true; //button reset is disabled
 
-    }
+    // задизабленность при обновлении страницы кнопок, скрытие блока с результатом
+  }
 
-    getCaloriesMax() {
+  _onFormSubmit(evt) {
+    // вызов методов расчета калорий
+    // getCaloriesNorm(), getCaloriesMin(), getCaloriesMax()
+    // показ блока с результатами калорий
+  }
 
-    }
+  init() {
+    this.element.addEventListener("change", (event) => {
+      event.preventDefault();
+      this._onFormInput(); //
+      // console.log("======");
+      // if (this.age || this.weight || this.height) {
+      //   this.btnReset.removeAttribute("disabled");
+      //   console.log(`Element is enabled`);
+      // }
+    });
+    this.btnReset.addEventListener("click", (event) => {
+      event.preventDefault();
+      this._onFormReset(); // Дуйствия при нажатии сброс
+    });
+
+    // инициализация обработчиков событий
+    // _onFormInput, _onFormReset, _onFormSubmit
+  }
+
+  deinit() {
+    // удаление обработчиков событий
+    // _onFormInput, _onFormReset, _onFormSubmit
+  }
+
+  getCaloriesNorm() {
+    // перечисление констант age, weight, height, gender, activity
+    // применение формулы расчета
+  }
+
+  getCaloriesMin() {}
+
+  getCaloriesMax() {}
 }
