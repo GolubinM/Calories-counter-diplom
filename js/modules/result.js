566aca3bb05ddb634960f5bc4c99e5a56432425d
Mihail Golubin <golm2020@gmail.com>
2022-12-30 12:22:42 +0300

    созданы методы для отображения/очистки результата

--- js/modules/result.js
+++ js/modules/result.js
@@ -1,18 +1,25 @@
 export default class Result {
-    constructor(element) {
-        this.counter = element;
+  constructor(element) {
+    this.counter = element;
 
-        this.root = element.querySelector(`.counter__result`);
-        this.caloriesNormOutput = this.root.querySelector(`#calories-norm`);
-        this.caloriesMinOutput = this.root.querySelector(`#calories-minimal`);
-        this.caloriesMaxOutput = this.root.querySelector(`#calories-maximal`);
-    }
+    this.root = element.querySelector(`.counter__result`);
+    this.caloriesNormOutput = this.root.querySelector(`#calories-norm`);
+    this.caloriesMinOutput = this.root.querySelector(`#calories-minimal`);
+    this.caloriesMaxOutput = this.root.querySelector(`#calories-maximal`);
+  }
 
-    show(calories) {
-        // показ блока с результатом
-    }
+  show(calories) {
+    this.root.classList.remove("counter__result--hidden"); // показ блока с результатом
+    this.caloriesNormOutput.textContent = Math.floor(calories.NORM + 0.5);
+    this.caloriesMinOutput.textContent = Math.floor(calories.MIN + 0.5);
+    this.caloriesMaxOutput.textContent = Math.floor(calories.MAX + 0.5);
+  }
 
-    hide() {
-        // скрытие блока, очистка значений
-    }
+  hide() {
+    this.root.classList.add("counter__result--hidden");
+    this.caloriesNormOutput.textContent = ''; // Добавлено согласно примечанию 'скрытие блока, очистка значений'
+    this.caloriesMinOutput.textContent = ''; 
+    this.caloriesMaxOutput.textContent = ''; 
+    // скрытие блока, очистка значений
+  }
 }
